ad0600eceaba31a37ab8ba40217e7f519c9b21b7
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-04-06 21:35:51 -0300

    feat: adicionado o método comprar produto

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -1,27 +1,36 @@
 import 'enums.dart';
 import 'pessoa.dart';
 import 'produto.dart';
+import 'revendedor.dart';
 
 class Cliente extends Pessoa {
   double dinheiro;
   List<Produto> produtosComprados = [];
 
-  Cliente({
-    required String nome,
-    required String cpf,
-    required DateTime dataDeNascimento,
-    required Genero genero,
-    double? dinheiro
-  }) : dinheiro = dinheiro ?? 0.0,
-       super(
-         nome: nome,
-         cpf: cpf,
-         dataDeNascimento: dataDeNascimento,
-         genero: genero
-       ); 
+  Cliente(
+      {required String nome,
+      required String cpf,
+      required DateTime dataDeNascimento,
+      required Genero genero,
+      double? dinheiro})
+      : dinheiro = dinheiro ?? 0.0,
+        super(
+            nome: nome,
+            cpf: cpf,
+            dataDeNascimento: dataDeNascimento,
+            genero: genero);
 
-       void adicionarDinheiro (double valor) {
-        dinheiro += valor;
+  void adicionarDinheiro(double valor) {
+    dinheiro += valor;
+  }
 
-       }
+  void comprarProduto(Produto produto, Revendedor revendedor) {
+    if (dinheiro >= produto.valor) {
+      revendedor.venderProduto(produto);
+      dinheiro -= produto.valor;
+      produtosComprados.add(produto);
+    } else {
+      print("$nome não possui dinheiro suficiente para comprar ${produto.nomeProduto}");
+    };
+  }
 }
