5e57d6790e363ecefc89801491f0b95eba6f6668
adrianafagundes <contato@adrianafagundes.com>
2024-04-15 12:02:34 -0300

    feat/fluttuando/calcularLucro

--- classes/revendedor.dart
+++ classes/revendedor.dart
@@ -1,32 +1,40 @@
-import 'pessoa.dart';
+import 'dart:ffi';
+
 import 'produto.dart';
 import 'enums.dart';
 
-class Revendedor extends Pessoa {
-  final String matricula;
+abstract class Revendedor extends Pessoa {
+  final matricula;
   List<Produto> produtosVendidos = [];
   double porcentagemLucro = 0.3;
 
-  Revendedor({
-    required this.matricula, 
-    required String nome, 
-    required String cpf, 
-    required DateTime dataDeNascimento,
-    required Genero genero
-  }) : super(nome: nome, cpf: cpf, dataDeNascimento: dataDeNascimento, genero: genero);
+  var ;
+  
+  static var cpf;
+  
+  static Null dataDeNascimento;
 
+  Revendedor (this.matricula, {required super.dataDeNascimento, required super.nome, required super.cpf, required super.genero} {this.matricula) : super('', null, dataDeNascimento: null, nome: '', , cpf: null, genero: null) {
+    
+  }
+  
+  get genero => null;
 
-
-  void falar(String falar) {
+  void falar(String falar), {
     
     switch (genero) {
       case Genero.Masculino:
+
         print('Revendedor $nome diz: $falar');
         break;
+
       case Genero.Feminino:
+
         print('Revendedora $nome diz: $falar');
         break;
+
       case Genero.Outro:
+
         print('Pessoa revendedora $nome diz: $falar');
         break;
     }
@@ -35,7 +43,8 @@ class Revendedor extends Pessoa {
  void venderProduto(Produto produto) {
   try {
     produto.realizarVenda();
-    this.produtosVendidos.add(produto);
+var produtosVendidos;
+produtosVendidos.add(produto);
   } catch (excecao) {
     throw excecao;
   }
@@ -44,14 +53,15 @@ class Revendedor extends Pessoa {
   double calcularTotalVendido() {
     double total = 0.0;
 
+    var produtosVendidos;
     produtosVendidos.forEach((produto) => total += produto.valor);
 
     return total;
   }
 
-
   double calcularMediaProdutosVendidos() {
     double total = 0.0;
+    var produtosVendidos;
     if (produtosVendidos.isEmpty) {
       return 0;
     }
@@ -60,4 +70,77 @@ class Revendedor extends Pessoa {
 
     return total / produtosVendidos.length;
   }
+}
+
+class implement {
+}
+
+ DynamicsCompressorNode(nome, cpf, {dynamicdataDeNascimento = null} {
+   // TODO: implement DynamicsCompressorNode
+   throw UnimplementedError();
+ }
+ class Pessoa {
+  String nome;
+  Int matricula;
+
+  Pessoa(this.nome, this.matricula, {required Comparable<DateTime> dataDeNascimento, required Comparable<String> nome, required Comparable<String> cpf, required Genero genero{dynmicrequired"nome", required Genero genero});
+}
+  
+    double calcularLucro() {
+    double lucroTotal = 0;
+    var precosVenda;
+    for (double preco in precosVenda) {
+      lucroTotal += preco;
+    }
+    return lucroTotal;
+  }
+
+  void calcularLucroResumo() {
+    double totalVendido = calcularLucro();
+    var precosVenda;
+    double media = totalVendido / precosVenda.length;
+    double lucro = calcularLucro();
+
+    print("O total vendido por $nome foi ${totalVendido.toStringAsFixed(2)} reais e a média aritmética de valor dos produtos vendidos é ${media.toStringAsFixed(2)} reais.");
+    print("O lucro recebido foi de ${lucro.toStringAsFixed(2)} reais.");
+  }
+
+void main() {
+  // Criação de um exemplo de lista de preços de venda
+  List<double> precosVendaJoao = [100, 150, 200, 250, 200, 200, 100, 100, 200, 226];
+
+  // Criação de uma instância de Revendedor para João
+  Revendedor1 joao = Revendedor1("João", precosVendaJoao, totalVendido);
+
+  // Chamada do método para calcular o lucro e exibir o resumo
+  joao.calcularLucroResumo();
+}
+
+mixin totalVendido {
+}
+
+class Revendedor1 {
+  Revendedor1(Comparable<String> s, List<double> precosVendaJoao, Type totalVendido);
+
+  @override
+  bool operator ==(Object other) {
+    // TODO: implement ==
+    return super == other;
+  }
+
+  Revendedor(String s, List<double> precosVendaJoao, Type totalVendido) async {
+
+    // TODO/ implement Revendedor1
+    throw UnimplementedError();
+  } @override
+  String toString() {
+    // TODO: implement toString
+    return super.toString();
+  }
+  void calcularLucroResumo() {
+
+  }
+}
+
+mixin generoString {
 }
